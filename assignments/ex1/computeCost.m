b06fbf4ba64e6f6447a1bc9f10e5cda49b3334a7
Pranai Vasudev <pranai.vasudev@gmail.com>
2020-09-14 15:03:52 -0700

    Implemented the cost function and gradient descent in ex1

--- assignments/ex1/computeCost.m
+++ assignments/ex1/computeCost.m
@@ -12,7 +12,9 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-
+for i=1:m
+  J = J + 1/(2*m)*(theta(1)*X(i,1) + theta(2)*X(i,2) - y(i))^2;
+endfor
 
 
 
--- assignments/ex1/gradientDescent.m
+++ assignments/ex1/gradientDescent.m
@@ -16,7 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
+    tempTheta1 = theta(1) - alpha*(1./m)*sum((X*theta - y).*X(:,1));
+    tempTheta2 = theta(2) - alpha*(1./m)*sum((X*theta - y).*X(:,2));
+    theta(1) = tempTheta1;
+    theta(2) = tempTheta2;
 
 
 
