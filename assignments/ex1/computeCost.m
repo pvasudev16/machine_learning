86fee511c5a3c380e62c8e8aafa1e05c6fa1ee97
Pranai Vasudev <pranai.vasudev@gmail.com>
2020-09-14 15:29:35 -0700

    ex1: did feature normalization

--- assignments/ex1/computeCost.m
+++ assignments/ex1/computeCost.m
@@ -12,9 +12,7 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
-for i=1:m
-  J = J + 1/(2*m)*(theta(1)*X(i,1) + theta(2)*X(i,2) - y(i))^2;
-endfor
+J = 1/(2*m)*sum((X*theta - y).^2);
 
 
 
--- assignments/ex1/featureNormalize.m
+++ assignments/ex1/featureNormalize.m
@@ -24,9 +24,12 @@ sigma = zeros(1, size(X, 2));
 %               each feature. 
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
-%       
-
-
+%
+for i=1:size(X,2)      
+  mu(1,i) = mean(X(:,i));
+  sigma(1,i) = std(X(:,i));
+  X_norm(:,i) = (X(:,i) - mu(1,i))/sigma(1,i);
+endfor
 
 
 
