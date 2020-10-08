fbf75080064b00c789225016c405ab21d0ef938c
Pranai Vasudev <pranai.vasudev@gmail.com>
2020-10-08 11:47:15 -0700

    Implemented forward propagation of n.n. to recoganize hand written digits

--- assignments/ex3/predict.m
+++ assignments/ex3/predict.m
@@ -10,6 +10,8 @@ num_labels = size(Theta2, 1);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
+X = [ones(m, 1) X];
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned neural network. You should set p to a 
@@ -20,6 +22,10 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+a2 = sigmoid(X*Theta1');
+a2 = [ones(size(a2,1), 1) a2];
+a3 = sigmoid(a2*Theta2');
+[max_vals, p] = max(a3, [], 2);
 
 
 
