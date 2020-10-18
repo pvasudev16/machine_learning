f1c4b3ef47792903fd00c07e90cf2e5885a11cd6
Pranai Vasudev <pranai.vasudev@gmail.com>
2020-10-17 19:01:31 -0700

    Done all but the last part of ex5

--- assignments/ex5/learningCurve.m
+++ assignments/ex5/learningCurve.m
@@ -42,14 +42,17 @@ error_val   = zeros(m, 1);
 %
 % Hint: You can loop over the examples with the following:
 %
-%       for i = 1:m
-%           % Compute train/cross validation errors using training examples 
-%           % X(1:i, :) and y(1:i), storing the result in 
-%           % error_train(i) and error_val(i)
-%           ....
-%           
-%       end
-%
+
+       for i = 1:m
+           % Compute train/cross validation errors using training examples 
+           % X(1:i, :) and y(1:i), storing the result in 
+           % error_train(i) and error_val(i)
+           [theta] = trainLinearReg(X(1:i,:), y(1:i), lambda);
+           error_train(i) = 1./(2.*i)*sum((X(1:i, :)*theta - y(1:i)).^2);
+           error_val(i) = 1./(2.*size(Xval,1))*sum((Xval*theta - yval).^2);
+       end
+       
+
 
 % ---------------------- Sample Solution ----------------------
 
