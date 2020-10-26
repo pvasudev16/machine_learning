dac01c6fe9e3f8edf4b6d8f08091b38a2c1e4c29
Pranai Vasudev <pranai.vasudev@gmail.com>
2020-10-26 11:09:13 -0700

    Did part one of assignment 6

--- assignments/ex6/dataset3Params.m
+++ assignments/ex6/dataset3Params.m
@@ -11,6 +11,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
+valVec = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+errorArray = zeros(size(valVec,1),size(valVec,1));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -22,8 +24,20 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
-
-
+for i=1:size(valVec,1)
+  for j=1:size(valVec,1)
+    cTemp = valVec(i);
+    sigmaTemp = valVec(j);
+    model= svmTrain(X, y, cTemp, @(x1, x2) gaussianKernel(x1, x2, sigmaTemp));
+    predictions = svmPredict(model, Xval);
+    errorArray(i,j) = mean(double(predictions~=yval));
+  endfor
+endfor
+%Get minimum of each column
+[J, indexC] = min(errorArray, [] ,1) ;
+[~, indexSigma ] = min(J, [], 2);
+C = valVec(indexC(indexSigma));
+sigma = valVec(indexSigma);
 
 
 
